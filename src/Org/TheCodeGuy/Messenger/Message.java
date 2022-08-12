940a9a50293e80c35dc72553b3c88ab8ad4b3081
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-08-12 09:13:27 +0500

    initial commit

---
+++ src/Org/TheCodeGuy/Messenger/Message.java
@@ -0,0 +1,40 @@
+package Org.TheCodeGuy.Messenger;
+
+public class Message {
+    private  String text;
+    private String recipient;
+    private int id;
+
+    public Message(String text, String recipient, int id) {
+        this.text = text;
+        this.recipient = recipient;
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void GetDetails(){
+        System.out.println("Contact Name : " + recipient+"\nMessage : "+text+"\nMessage ID : "+id);
+    }
+}
