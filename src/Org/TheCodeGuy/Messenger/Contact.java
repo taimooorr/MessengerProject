786a7e0da3bc47785a32a66d1d53b57eddfea44d
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-08-12 09:41:19 +0500

    Added Managing Contacts

---
+++ src/Org/TheCodeGuy/Messenger/Contact.java
@@ -0,0 +1,62 @@
+package Org.TheCodeGuy.Messenger;
+
+import java.util.ArrayList;
+
+public class Contact {
+    private String name;
+    private String number;
+    private String email;
+    private ArrayList<Message> messages;
+
+    public Contact(String name, String number, String email, ArrayList<Message> messages) {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+        this.messages = messages;
+    }
+
+    public Contact(String name, String number, String email) {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+        this.messages = new ArrayList<Message>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(ArrayList<Message> messages) {
+        this.messages = messages;
+    }
+public void GetDetails(){
+        System.out.println("Contact Name : " + name+
+                "\nContact Number : "+number+
+                "\nContact Email : "+email);
+    }
+
+}
---
+++ src/Org/TheCodeGuy/Messenger/Main.java
@@ -0,0 +1,143 @@
+package Org.TheCodeGuy.Messenger;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    private static ArrayList<Contact> contacts;
+    private static Scanner scanner;
+    private static void showOptions() {
+        System.out.println("1. Manage Contact");
+        System.out.println("2. Send Message");
+        System.out.println("3. Exit");
+        scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                manageContact();
+                break;
+            case 2:
+                sendMessage();
+                break;
+            case 3:
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid Option");
+                showOptions();
+                break;
+        }
+
+    }
+    private static void manageContact() {
+        System.out.println("1. Show All Contacts"+
+                "\n2. Add Contact"+
+                "\n3. Search for a Contact"+
+                "\n4. Delete Contact"+
+                "\n5. Back");
+        int option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                showAllContacts();
+                break;
+            case 2:
+                addContact();
+                break;
+            case 3:
+                searchContact();
+                break;
+            case 4:
+                deleteContact();
+                break;
+            case 5:
+                showOptions();
+                break;
+            default:
+                System.out.println("Invalid Option");
+                manageContact();
+                break;
+        }
+
+    }
+
+    private static void deleteContact() {
+        System.out.println("Enter Contact Name to Delete");
+        String name = scanner.next();
+        if(name.equals("")){
+            System.out.println("Can not Search Null Value");
+            deleteContact();
+        }
+        else{
+            boolean found = false;
+            for (Contact contact : contacts) {
+                if (contact.getName().equals(name)) {
+                    found = true;
+                    contacts.remove(contact);
+                    System.out.println("Contact Deleted");
+                    break;
+                }
+            }
+            if (!found) {
+                System.out.println("Contact Not Found");
+            }
+        }
+        manageContact();
+    }
+
+    private static void searchContact() {
+        System.out.println("Enter Contact Name to Search");
+        String name = scanner.next();
+        if(name.equals("")){
+            System.out.println("Invalid Name");
+            searchContact();
+        }
+        else{
+            boolean doesExist = false;
+            for(Contact contact : contacts){
+                if(contact.getName().equals(name)){
+                    System.out.println("Contact Found");
+                    doesExist = true;
+                    contact.GetDetails();
+
+                }
+            }
+            if(!doesExist){
+                System.out.println("Contact Not Found");
+            }
+
+        }
+        manageContact();
+    }
+
+    private static void addContact() {
+        System.out.println("Enter Contact Name");
+        String name = scanner.next();
+        System.out.println("Enter Contact Number");
+        String number = scanner.next();
+        System.out.println("Enter Contact Email");
+        String email = scanner.next();
+        if(name.equals("") || number.equals("") ){
+            System.out.println("Please Enter Valid Details");
+            addContact();
+        }
+        else{
+            Contact contact = new Contact(name, number, email);
+            contacts.add(contact);
+            System.out.println("Contact Added Successfully");
+
+        }
+        manageContact();
+    }
+
+    private static void showAllContacts() {
+        for (Contact contact : contacts) {
+            contact.GetDetails();
+        }
+        manageContact();
+    }
+
+    public static void main(String[] args) {
+        contacts = new ArrayList<Contact>();
+        System.out.println("Welcome to the Messenger App");
+    }
+}
