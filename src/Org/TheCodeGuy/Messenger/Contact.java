08badfb3423f0e82df7876b9f688aea680634607
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-08-13 23:28:58 +0500

    fixed bug

--- src/Org/TheCodeGuy/Messenger/Contact.java
+++ src/Org/TheCodeGuy/Messenger/Contact.java
@@ -26,24 +26,8 @@ public class Contact {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getNumber() {
         return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public ArrayList<Message> getMessages() {
